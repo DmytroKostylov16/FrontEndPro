28ab863f3524a9ebc78df85a272b34eef1d46be8
Dmytro Kostylov <dimakostylov1609@gmail.com>
2025-02-23 11:08:18 +0100

    l3hw2

---
+++ main.js
@@ -0,0 +1,5 @@
+let userName = prompt('What is your name?');
+let userAge=prompt("How old are you?");
+let userFavoriteColor = prompt('What is you favorite color?');
+let answer = `Hello, ${userName}, you are ${userAge} years old, ${userFavoriteColor} is my favorite color too!Nice too meet you.`;
+alert(answer);
