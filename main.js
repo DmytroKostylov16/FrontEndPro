08a6a3e5c739ae65750e30e8a381721c6a96f43a
Dmytro Kostylov <dimakostylov1609@gmail.com>
2025-02-23 10:16:51 +0100

    l3hw1

---
+++ main.js
@@ -0,0 +1,18 @@
+let number = 28;
+let bigInt = 12345678910111213n;
+let string = 'JavaScript';
+let boolean = true;
+let nul = null;
+let und;
+let symbol = Symbol("sym");
+
+console.log(number);
+console.log(bigInt);
+console.log(string);
+console.log(boolean);
+console.log(nul);
+console.log(und);
+console.log(symbol);
+
+console.log(typeof(number));
+console.log(typeof(string));
