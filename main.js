c05087a3e00704ebf03be6c8aa9ac713c7ee4aa1
Dmytro Kostylov <dimakostylov1609@gmail.com>
2025-03-18 10:35:00 +0100

    l8hw1

---
+++ main.js
@@ -0,0 +1,17 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step += 1;
+        return this;
+    },
+    down: function () {
+        this.step -= 1;
+        return this;
+    },
+    showStep: function () {
+        console.log(this.step);
+        return this;
+    }
+};
+
+ladder.up().up().down().showStep().up().up().down().up().showStep();
